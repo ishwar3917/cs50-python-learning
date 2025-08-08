66ecfd9486afa0fa6bca5dc9afb7ae994329e4a0
Ishwar Soni <ishwarsoni2917@gmail.com>
2025-08-08 17:59:40 +0530

    Add hello.py for Week 0

---
+++ week0_intro/hello.py
@@ -0,0 +1,8 @@
+print("Hello,world")
+
+# "#" is use for the single linne comments 
+"""" And these are use for the multiline comments
+"""
+# it is for the  user to make notes and for other person to understand the code 
+
+# print is a function which helps us to print anything within these parantheses (brackets) 
