69b90c737f2f1594bc181922a3eb93bcb648d4af
Ishwar Soni <ishwarsoni2917@gmail.com>
2025-08-08 18:00:50 +0530

    Add hello.py for Week 0

--- week0_intro/hello.py
+++ week0_intro/hello.py
@@ -1,6 +1,6 @@
 print("Hello,world")
 
-# "#" is use for the single linne comments 
+# "#" is use for the single line comments 
 """" And these are use for the multiline comments
 """
 # it is for the  user to make notes and for other person to understand the code 
